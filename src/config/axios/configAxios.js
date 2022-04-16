00a48e06cdcbceeddf9bc110e799202e6048a46d
Jerome <ducampdorgasjerome@gmail.com>
2022-04-16 12:30:57 +0200

    remise headers

--- src/config/axios/configAxios.js
+++ src/config/axios/configAxios.js
@@ -4,9 +4,9 @@ const token = localStorage.getItem("token");
 
 const instance = axios.create({
   baseURL: "https://lsf-back-2.herokuapp.com/api/", //"http://localhost:3000/api/",
-  /*headers: {
+  headers: {
     Authorization: token,
-  },*/
+  },
 });
 
 export default instance;
